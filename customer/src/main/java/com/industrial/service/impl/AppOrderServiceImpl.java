f5d53700e047d9cf4d9462f4815deae5a5f71d2e
lishenkang <657507210@qq.com>
2022-01-27 16:04:28 +0800

    审核接口层

--- customer/src/main/java/com/industrial/service/impl/AppOrderServiceImpl.java
+++ customer/src/main/java/com/industrial/service/impl/AppOrderServiceImpl.java
@@ -1,6 +1,8 @@
 package com.industrial.service.impl;
 
-import com.industrial.common.annotation.Log;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.industrial.common.core.domain.entity.SysDept;
+import com.industrial.common.core.domain.entity.SysUser;
 import com.industrial.common.dto.CheckDto;
 import com.industrial.common.dto.OrderDto;
 import com.industrial.common.exception.ServiceException;
@@ -8,8 +10,9 @@ import com.industrial.common.vo.OrderVo;
 import com.industrial.domin.*;
 import com.industrial.mapper.*;
 import com.industrial.service.AppOrderService;
+import com.industrial.system.mapper.SysDeptMapper;
+import com.industrial.system.mapper.SysUserMapper;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
@@ -38,6 +41,13 @@ public class AppOrderServiceImpl implements AppOrderService {
     private AppCheckMainConfigMapper mainConfigMapper;
     @Resource
     private AppOrderLogMapper orderLogMapper;
+    @Resource
+    private SysUserMapper sysUserMapper;
+    @Resource
+    private AppCheckUserMapper appCheckUserMapper;
+    @Resource
+    private SysDeptMapper deptMapper;
+
 
     @Override
     public OrderDto selectById(Integer orderId) {
@@ -99,7 +109,12 @@ public class AppOrderServiceImpl implements AppOrderService {
         return orderMapper.updateById(order)==1;
     }
 
-
+    /**
+     * 订单审核  关联审核设置，订单动态，审核 返回为空字符串为成功
+     * @param userId 用户id
+     * @param orderNo 订单编号
+     * @return
+     */
     @Transactional(rollbackFor = ServiceException.class)
     public java.lang.String checkOrder(long userId, java.lang.String orderNo) {
         StringBuilder msg=new StringBuilder();
@@ -113,16 +128,18 @@ public class AppOrderServiceImpl implements AppOrderService {
                 msg.append("找不到审核的配置信息");
                 return msg.toString();
             }
-            // 审核配置关闭，插入信息默认审核通过
+            SysUser curUser=sysUserMapper.selectUserById(userId);
+            if(curUser==null){
+                msg.append("找不到对应用户");
+                return msg.toString();
+            }
             if(config.stream().findFirst().orElse(null).getCheckStatus()==1){
+                //region 审核配置关闭，插入信息默认审核通过
                 if(model!=null&&model.getId()>=0){
                     model.setStatus(1);
                     model.setUpdateTime(new Date());
                     appCheckMapper.updateAppCheck(model);
-                    AppOrderLog log =new AppOrderLog();
-                    log.setOrderNo(orderNo);
-                    log.setStatus(1);
-                    orderLogMapper.insertAppOrderLog(log);
+
                 }else{
                     model=new AppCheck();
                     model.setOrderId(orderNo);
@@ -135,12 +152,27 @@ public class AppOrderServiceImpl implements AppOrderService {
                     model.setCreateTime(new Date());
                     appCheckMapper.insertAppCheck(model);
                 }
+                //region 订单动态
+                AppOrderLog log =new AppOrderLog();
+                log.setOrderNo(orderNo);
+                log.setStatus(1);
+                log.setLevel(0);
+                log.setUserId(userId);
+                log.setStatus(1);
+                log.setRemark(curUser.getNickName()+":审核配置关闭，自动审核通过。");
+                log.setCreateTime(new Date());
+                log.setUpdateTime(new Date());
+                orderLogMapper.insertAppOrderLog(log);
+                //endregion
+                return msg.toString();
+                //endregion
+
             }
             //endregion
             if(model==null){
 //            msg.append("找不到该订单的审核信息");
-                //region 审核数据 插入 审核id写死为1 层极写死为1
-                CheckDto oneClass=config!=null?config.stream().filter(item->item.clevel==1).collect(Collectors.toList()).stream().findFirst().orElse(null):null;
+                //region 审核数据 插入 审核id写死为1 层极写死为1 model重新赋值
+                CheckDto oneClass=config.stream().filter(item->item.clevel==1).collect(Collectors.toList()).stream().findFirst().orElse(null);
                 if(oneClass==null){
                     msg.append("找不到层级1的审核的配置信息");
                     return msg.toString();
@@ -155,6 +187,7 @@ public class AppOrderServiceImpl implements AppOrderService {
                 model.setUpdateTime(new Date());
                 model.setCreateTime(new Date());
                 appCheckMapper.insertAppCheck(model);
+                model=appCheckMapper.selectAppCheckByOrderNo(orderNo);
                 //endregion
             }
             if(model.getStatus()==1){
@@ -166,7 +199,83 @@ public class AppOrderServiceImpl implements AppOrderService {
                 return  msg.toString();
             }
             //正常检测操作
-
+            int level=model.getAuditLevel().intValue();
+            int auditId=model.getAuditId().intValue();
+            CheckDto cur=config.stream().filter(item->item.clevel==level&&item.getId()==auditId).collect(Collectors.toList()).stream().findFirst().orElse(null);
+            if(cur==null){
+                msg.append("找不到该层的审核配置信息 层级="+level+"|id="+auditId);
+                return msg.toString();
+            }
+            //1.负责人主管
+            //2.多人中一人
+            //3.多人会签
+            //4.上级审核人
+            //region 权限判断
+            boolean curFlag=false;
+            if(cur.getCheckType()==1){
+                List<SysUser> upUserList= sysUserMapper.selectUpUserList((curUser.getDeptId()+","+curUser.getDept().getAncestors()).split(","));
+                if(upUserList.stream().filter(x->x.getUserId()==userId).collect(Collectors.toList()).size()>0){
+                    curFlag=true;
+                }
+            }else if(cur.getCheckType()==2){
+                QueryWrapper<AppCheckUser> qw = new QueryWrapper<>();
+                qw.lambda().eq(com.industrial.domin.AppCheckUser::getCheckId, cur.cid);
+                List<AppCheckUser> upUserList=appCheckUserMapper.selectList(qw);
+                if(upUserList.stream().filter(x->x.getUserId()==userId).collect(Collectors.toList()).size()>0){
+                    curFlag=true;
+                }
+            }else if(cur.getCheckType()==3){
+                QueryWrapper<AppCheckUser> qw = new QueryWrapper<>();
+                qw.lambda().eq(com.industrial.domin.AppCheckUser::getCheckId, cur.cid);
+                List<AppCheckUser> upUserList=appCheckUserMapper.selectList(qw);
+                List<AppOrderLog> hadUserList=orderLogMapper.selectOrderLogListForCheck(cur.clevel,orderNo);
+                if(upUserList.size()==hadUserList.size()){
+                    curFlag=true;
+                }
+            }else if(cur.getCheckType()==4){
+                SysDept dept= deptMapper.selectDeptById(curUser.getDept().getParentId());
+                if(dept.getLeader()==String.valueOf(userId)){
+                    curFlag=true;
+                }
+            }
+            //endregion
+            //region 是否完成该审核 是否有下一级
+            if(curFlag){
+                CheckDto nextConfig=config.stream().filter(item->item.clevel==(level+1)&&item.getId()==auditId).collect(Collectors.toList()).stream().findFirst().orElse(null);
+                if(nextConfig==null){
+                    model.setStatus(1);
+                    model.setUpdateTime(new Date());
+                    appCheckMapper.updateAppCheck(model);
+                    //region 订单动态
+                    AppOrderLog log =new AppOrderLog();
+                    log.setOrderNo(orderNo);
+                    log.setStatus(1);
+                    log.setLevel(cur.clevel);
+                    log.setUserId(userId);
+                    log.setRemark(curUser.getNickName()+":审核通过。开始生成工单");
+                    log.setCreateTime(new Date());
+                    log.setUpdateTime(new Date());
+                    orderLogMapper.insertAppOrderLog(log);
+                    //endregion
+                    // 这里缺工单生成
+                }else{
+                    model.setAuditLevel(Long.valueOf(nextConfig.clevel));
+                    model.setUpdateTime(new Date());
+                    appCheckMapper.updateAppCheck(model);
+                    //region 订单动态
+                    AppOrderLog log =new AppOrderLog();
+                    log.setOrderNo(orderNo);
+                    log.setStatus(1);
+                    log.setLevel(cur.clevel);
+                    log.setUserId(userId);
+                    log.setRemark(curUser.getNickName()+":审核通过。进入第"+nextConfig.clevel+"级审核");
+                    log.setCreateTime(new Date());
+                    log.setUpdateTime(new Date());
+                    orderLogMapper.insertAppOrderLog(log);
+                    //endregion
+                }
+            }
+            //endregion
             return  msg.toString();
 
         }catch (Exception ex){
